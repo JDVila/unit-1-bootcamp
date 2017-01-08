a346615857ea18c59130b32f5d06b49c5b948552
Jose Vila <you@example.comJDVila@gmail.com>
2017-01-08 00:00:12 -0500

    created API branch, no content, just TODOs

--- src/UsingAPIs.java
+++ src/UsingAPIs.java
@@ -1,9 +1,14 @@
+import java.util.HashSet;
+
 /**
  * Created by alexandraqin on 4/14/15.
  */
+
 public class UsingAPIs {
+    static HashSet<String> hashSet = new HashSet();
 
   public static void main (String args[]) {
-
+      //TODO - COMPLETE HASHSET QUESTION
+      //TODO - COMPLETE STRIPE API QUESTION
   }
 }
